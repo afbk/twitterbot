5a0e1e12dd435732f6ce4bd15407407b87f98695
Anders Folkmar Belling Kraneled <anderskraneled@gmail.com>
2017-11-27 09:14:15 +0000

    Added initial tests for CreateArrayOfWords()

---
+++ test/CreateArrayOfWordsTest.js
@@ -0,0 +1,10 @@
+let mocha = require("mocha");
+let chai = require("chai");
+let expect = chai.expect;
+let CreateArrayOfWords = require("../src/CreateArrayOfWords.js");
+
+describe("Creates an array of all words recieved from ReadTweets()", () => {
+    it("should return an array");
+    it("should return an array of larger size than input");
+    it("should return a string if no array is input");
+});
