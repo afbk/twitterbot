7159078c40093bd54c30435629c17879a72c06d3
Anders Folkmar Belling Kraneled <anderskraneled@gmail.com>
2018-01-03 16:54:56 +0000

    sheesh

---
+++ src/twitterCredentials.js
@@ -0,0 +1,8 @@
+let twitterCredentials = {
+  consumer_key: '4Jwf0DcfdyynqVb05lS4eFFER',
+  consumer_secret: 'CPD07MZ97vuQig2ZXDzbV6rGuN1UiKvjIj6XCxGOxkAXc1qEzZ',
+  access_token: '948584942103080960-BpyHlpsJxCWlW5TQfroXFbWzkJlhP54',
+  access_token_secret: 'd3PSS1irn7KzWEHeaMd3rLPqIBy01M3bX8leSg9QhHrXD'
+}
+
+module.exports = twitterCredentials
